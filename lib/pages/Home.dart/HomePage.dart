8ac392d80ddc5cb619cf1731ab9835330c78f4c7
SUMIT KUMAR <dhruvanwd@gmail.com>
2021-08-14 01:31:50 +0530

    Component seperator

--- lib/pages/Home.dart/HomePage.dart
+++ lib/pages/Home.dart/HomePage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:orca_mob/GetxControllers/GlobalController.dart';
 
+import 'ShopByCategory.dart';
+
 class HomePage extends StatelessWidget {
   final globalController = Get.find<GlobalController>();
   HomePage({Key? key}) {
@@ -25,50 +27,7 @@ class HomePage extends StatelessWidget {
         ),
         body: Container(
           child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 4),
-                child: Text(
-                  "Shop by category",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              SizedBox(
-                height: 90,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: _.categories
-                      .map(
-                        (e) => Card(
-                          clipBehavior: Clip.hardEdge,
-                          color: Colors.grey.shade300,
-                          margin: EdgeInsets.symmetric(horizontal: 4),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(4)),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: Image(
-                                  image: NetworkImage(
-                                    _.createImageUrl(e['image']),
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(bottom: 8),
-                                child: Center(
-                                  child: Text(e['name']),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                      .toList(),
-                ),
-              )
-            ],
+            children: [ShopByCategory()],
           ),
         ),
       ),
---
+++ lib/pages/Home.dart/ShopByCategory.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:orca_mob/GetxControllers/GlobalController.dart';
+
+class ShopByCategory extends StatelessWidget {
+  ShopByCategory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<GlobalController>(
+      builder: (_) => Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 8),
+            child: Text(
+              "Shop by category",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          SizedBox(
+            height: 90,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: _.categories
+                  .map(
+                    (e) => Card(
+                      clipBehavior: Clip.hardEdge,
+                      color: Colors.grey.shade200,
+                      margin: EdgeInsets.symmetric(horizontal: 4),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Image(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                _.createImageUrl(e['image']),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(bottom: 8),
+                            child: Center(
+                              child: Text(e['name']),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
